fc425187f7636c23c531d28b884d98e0dafc6080
NiclasOlofsson <nicke.olofsson@gmail.com>
2017-11-05 02:53:33 +0100

    Fix attribute for owner

--- AStarNavigator/Properties/AssemblyInfo.cs
+++ AStarNavigator/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AStar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("github/NiclasOlofsson/astar-navigator")]
 [assembly: AssemblyProduct("AStar")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
